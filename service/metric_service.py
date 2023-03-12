59276da905aed036ef841ee37b85132d53ca6068
Mouse* <narek.mouse@gmail.com>
2023-03-12 16:29:02 +0400

    resolve #2 - add in memory registery for tmp use and testing (#3)

--- service/metric_service.py
+++ service/metric_service.py
@@ -2,14 +2,15 @@ from typing import List, Union
 
 from model import Metric
 from service import MetricServiceInterface
+from registery import TSDBInterface
 
 
 class MetricService(MetricServiceInterface):
-    def __init__(self):
-        self.metrics: List[Metric] = []
+    def __init__(self, tsdb: TSDBInterface):
+        self.tsdb = tsdb
 
     def add_metric(self, metric: Metric):
-        self.metrics.append(metric)
+        self.tsdb.add_metric(metric)
 
     def get_last_metric(self) -> Union[Metric, None]:
-        return self.metrics[-1] if len(self.metrics) else None
+        return self.tsdb.get_last_metric()
--- service/test_metric_service.py
+++ service/test_metric_service.py
@@ -1,21 +1,22 @@
 from model import Metric
 from service import MetricService
+from registery import InMemoryTSDB
 
 
 def test_init():
-    service = MetricService()
+    service = MetricService(InMemoryTSDB())
     assert service.get_last_metric() is None
 
 
 def test_get_first():
-    service = MetricService()
+    service = MetricService(InMemoryTSDB())
     metric = Metric(name="RH", unit="%", value=65.34)
     service.add_metric(metric)
     assert service.get_last_metric() == metric
 
 
 def test_second_first():
-    service = MetricService()
+    service = MetricService(InMemoryTSDB())
     first_metric = Metric(name="RH", unit="%", value=65.34)
     second_metric = Metric(name="Temp", unit="C", value=65.34)
     service.add_metric(first_metric)
